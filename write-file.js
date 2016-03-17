f7427d885248b7478c41652e9c02596432d4965e
nanzhangren <trustfei@gmail.com>
2016-03-17 23:42:14 +0800

    Add write stream file

---
+++ write-file.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var data = 'Here you are. It is a nice place.';
+
+var writeStream = fs.createWriteStream('input.txt');
+
+writeStream.write(data, 'UTF8');
+
+writeStream.end();
+
+writeStream.on('finish', function() {
+    console.log('Write finished!');
+});
+
+writeStream.on('error', function(error) {
+    console.log(error.stack);
+});
+
+console.log('Progress end.');
