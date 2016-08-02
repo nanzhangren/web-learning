8afafdb422a3531a71f160db0ae3a51994cdcbbe
nanzhangren <trustfei@gmail.com>
2016-08-02 14:16:32 +0800

    Create main page.

--- reactJS/source/main.js
+++ reactJS/source/main.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 var ChoiceButton = React.createClass({
     render: function () {
@@ -7,55 +7,73 @@ var ChoiceButton = React.createClass({
             width: this.props.btnSize,
             height: this.props.btnSize,
             margin: 8,
-            backgroundColor: this.props.btnColor
+            display: "inline-block"
+        };
+
+        var btnStyle = {
+            width: "100%",
+            height: "100%",
+            backgroundColor: this.props.btnColor,
+            borderColor: "transparent",
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "large"
         };
 
         return (
             <div style={divStyle}>
-                <input type="button" value={this.props.btnValue} />
+                <input type="button" value={this.props.btnValue} style={btnStyle} />
             </div>
         );
     }
 });
 
-var FrameDiv = React.createClass({
+var ButtonRow = React.createClass({
     render: function () {
         var divStyle = {
-            // float: this.props.frameDivPosition,
-            display: "inline-block",
-            width: this.props.frameDivWidth,
-            height: "100%",
-            margin: 0,
-            backgroundColor: this.props.frameDivColor
+            textAlign: "center",
+            marginTop: "30px",
+            marginBottom: "30px"
         };
-
+        var btnSize = 160;
         return (
-            <div style={divStyle}></div>
+            <div style={divStyle}>
+                <ChoiceButton btnSize={btnSize} btnColor="blue" btnValue="缴费" />
+                <ChoiceButton btnSize={btnSize} btnColor="red" btnValue="查询" />
+                <ChoiceButton btnSize={btnSize} btnColor="green" btnValue="社区活动" />
+            </div>
+        );
+    }
+});
+
+var TextShown = React.createClass({
+    render: function () {
+        return (
+            <div style={{ textAlign: "center", margin: this.props.textMargin }}>
+                <span style={{ fontSize: this.props.textSize, fontWeight: "bold" }}>{this.props.textContent}</span>
+            </div>
         );
     }
 });
 
 var App = React.createClass({
     render: function () {
-        var divStyle = {
-            width: "100%",
-            height: this.props.height
-        };
-
         return (
-            <div style={divStyle}>
-                <FrameDiv frameDivWidth="20%" frameDivColor="red" />
-                <FrameDiv frameDivWidth="60%" frameDivColor="lightblue">
-                    <ChoiceButton btnSize="210px" btnColor="red" btnValue="Pay" />
-                    <ChoiceButton btnSize="210px" btnColor="green" btnValue="Find" />
-                </FrameDiv>
-                <FrameDiv frameDivWidth="20%" frameDivColor="yellowgreen" />
+            <div style={{height: this.props.appHeight}}>
+                <div className="frame-div" style={{ width: "20%", backgroundColor: "red" }}></div>
+                <div className="frame-div" style={{ width: "60%", backgroundColor: "yellowgreen" }}>
+                    <TextShown textContent="xxx小区管理系统" textSize="x-larger" textMargin="18px" />
+                    <div style={{ marginTop: "180px" }}>
+                        <ButtonRow />
+                    </div>
+                </div>
+                <div className="frame-div" style={{ width: "20%", backgroundColor: "lightblue" }}></div>
             </div>
         );
     }
 });
 
 ReactDOM.render(
-    <App height="800px" />,
-    document.getElementById('sample')
+    <App appHeight="800px" />,
+    document.getElementById("sample")
 );
