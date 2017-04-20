432304ac0fb38abe4a667fe46535b3fb3cb22f0a
nanzhangren <trustfei@gmail.com>
2017-04-21 00:01:16 +0800

    Add service to angular2 sample.

--- angular2/src/app/app.component.ts
+++ angular2/src/app/app.component.ts
@@ -1,20 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -77,15 +64,28 @@ const HEROES: Hero[] = [
             </li>
         </ul>
         <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    `
+    `,
+    providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    constructor(private heroService: HeroService) {        
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
     }
 }
