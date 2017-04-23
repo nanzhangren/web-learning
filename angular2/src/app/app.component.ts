44d18a716fdc5b52fadad82ec69e9b3f52942a2b
nanzhangren <trustfei@gmail.com>
2017-04-23 22:22:00 +0800

    Add router to angular2 sample.

--- angular2/src/app/app.component.ts
+++ angular2/src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <my-heroes></my-heroes>
+        <nav>
+            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
     `
 })
 
