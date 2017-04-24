597d3ba08d20382394d1d53785ff240505e0dd85
nanzhangren <trustfei@gmail.com>
2017-04-24 23:00:59 +0800

    Add http to angular2 sample.

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -14,7 +18,9 @@ import {AppRoutingModule } from './app-routing.module';
     imports: [
         BrowserModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
     ],
     declarations: [
         AppComponent,
